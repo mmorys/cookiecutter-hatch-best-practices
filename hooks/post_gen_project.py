754e4f2b4cf695c60ca3dd4873350bcd184cad64
Marcin Morys <marcin.morys.1@us.af.mil>
2022-10-07 11:10:28 -0400

    fix(post_gen_project.py): fix scoping of commands to remove cli and tests dirs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,21 +1,22 @@
 """cookiecutter-hatch-best-practices post-gen cookiecutter hook script."""
 
+import os
+import shutil
+
 src_layout = "{{ cookiecutter.src_layout }}"
 use_cli = "{{ cookiecutter.use_cli }}"
 use_tests = "{{ cookiecutter.use_tests }}"
 
+# Remove cli directory if not needed
+if not use_cli:
+    shutil.rmtree("{{ cookiecutter.__package_name }}/cli")
+
+# Remove tests directory if not needed
+if not use_tests:
+    shutil.rmtree("tests")
+
+# Move package to src directory
+# *Must happen af the end of other file modifications
 if src_layout:
-    import os
-    import shutil
-
-    # Remove cli directory if not needed
-    if not use_cli:
-        shutil.rmtree("{{ cookiecutter.__package_name }}/cli")
-
-    if not use_tests:
-        shutil.rmtree("tests")
-
-    # Move package to src directory
-    # *Must happen af the end of other file modifications
     os.mkdir("src")
     shutil.move("{{ cookiecutter.__package_name }}", "src")
