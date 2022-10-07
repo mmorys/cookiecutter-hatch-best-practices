11897dd633bb2e95c80555c2b9dc96bdf34bc652
Marcin Morys <marcin.morys.1@us.af.mil>
2022-10-07 12:57:55 -0400

    feat(precommit): add option to automate git and precommit setup

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,7 @@ import shutil
 src_layout = "{{ cookiecutter.src_layout }}"
 use_cli = "{{ cookiecutter.use_cli }}"
 use_tests = "{{ cookiecutter.use_tests }}"
+git_and_precommit_init = "{{ cookiecutter.git_and_precommit_init }}"
 
 # Remove cli directory if not needed
 if not use_cli:
@@ -16,7 +17,14 @@ if not use_tests:
     shutil.rmtree("tests")
 
 # Move package to src directory
-# *Must happen af the end of other file modifications
+# *Must happen at the end of other file modifications
 if src_layout:
     os.mkdir("src")
     shutil.move("{{ cookiecutter.__package_name }}", "src")
+
+# Initialize git and install pre-commit hooks
+if git_and_precommit_init:
+    import subprocess
+
+    subprocess.Popen(["git", "init"])
+    subprocess.Popen(["hatch", "run", "pre-commit", "install"])
